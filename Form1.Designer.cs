588d496f831b287bacb4bfaa7bca1ab8f7adcd90
Мария Плетнева <mspletneva_1@edu.hse.ru>
2022-03-09 21:55:10 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace MainApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.filtersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.plugindInfoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filtersToolStripMenuItem,
+            this.plugindInfoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(533, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // filtersToolStripMenuItem
+            // 
+            this.filtersToolStripMenuItem.Name = "filtersToolStripMenuItem";
+            this.filtersToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.filtersToolStripMenuItem.Text = "Filters";
+            // 
+            // plugindInfoToolStripMenuItem
+            // 
+            this.plugindInfoToolStripMenuItem.Name = "plugindInfoToolStripMenuItem";
+            this.plugindInfoToolStripMenuItem.Size = new System.Drawing.Size(88, 20);
+            this.plugindInfoToolStripMenuItem.Text = "PluginsInfo...";
+            this.plugindInfoToolStripMenuItem.Click += new System.EventHandler(this.plugindInfoToolStripMenuItem_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
+            this.pictureBox.Location = new System.Drawing.Point(12, 37);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(509, 380);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 440);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem filtersToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ToolStripMenuItem plugindInfoToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using PluginInterface;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace MainApp
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, IPlugin> plugins = new Dictionary<string, IPlugin>();
+        public Form1()
+        {
+            InitializeComponent();
+            FindPlugins();
+            CreatePluginsMenu();
+        }
+
+        void CreatePluginsMenu()
+        {
+            foreach (IPlugin p in plugins.Values)
+            {
+                var menuItem = new ToolStripMenuItem(p.Name);
+                menuItem.Click += OnPluginClick;
+
+                filtersToolStripMenuItem.DropDownItems.Add(menuItem);
+            }
+
+        }
+
+        void FindPlugins()
+        {
+            // папка с плагинами
+            string folder = System.AppDomain.CurrentDomain.BaseDirectory;
+            string projectDirectory = Directory.GetParent(folder).Parent.Parent.FullName;
+            string[] files;
+            if (File.Exists(projectDirectory + "\\App.config"))
+            {
+                foreach (string x in ConfigurationManager.AppSettings)
+                {
+                    files = Directory.GetFiles(ConfigurationManager.AppSettings.Get(x), "*.dll");
+                    foreach (string file in files)
+                        try
+                        {
+                            Assembly assembly = Assembly.LoadFile(file);
+
+                            foreach (Type type in assembly.GetTypes())
+                            {
+                                Type iface = type.GetInterface("PluginInterface.IPlugin");
+
+                                if (iface != null)
+                                {
+                                    IPlugin plugin = (IPlugin)Activator.CreateInstance(type);
+                                    plugins.Add(plugin.Name, plugin);
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Ошибка загрузки плагина\n" + ex.Message);
+                        }
+                }
+            }
+            else
+            {// dll-файлы в этой папке
+                files = Directory.GetFiles(folder, "*.dll");
+
+                foreach (string file in files)
+                    try
+                    {
+                        Assembly assembly = Assembly.LoadFile(file);
+
+                        foreach (Type type in assembly.GetTypes())
+                        {
+                            Type iface = type.GetInterface("PluginInterface.IPlugin");
+
+                            if (iface != null)
+                            {
+                                IPlugin plugin = (IPlugin)Activator.CreateInstance(type);
+                                plugins.Add(plugin.Name, plugin);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Ошибка загрузки плагина\n" + ex.Message);
+                    }
+            }
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void OnPluginClick(object sender, EventArgs args)
+        {
+            IPlugin plugin = plugins[((ToolStripMenuItem)sender).Text];
+            plugin.Transform((Bitmap)pictureBox.Image);
+            pictureBox.Refresh();
+        }
+
+        private void plugindInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string res = "Подключенные плагины\n\n";
+            foreach (var x in plugins)
+            {
+                res += "Имя: " + x.Value.Name + "\nАвтор: " + x.Value.Author;
+                res += "\nВерсия: " + GetAttribute(x.Value.GetType()) + "\n\n";
+            }
+            MessageBox.Show(res);
+        }
+        public static string GetAttribute(Type t)
+        {
+            VersionAttribute MyAttribute =
+                (VersionAttribute)Attribute.GetCustomAttribute(t, typeof(VersionAttribute));
+
+            if (MyAttribute == null)
+            {
+                return "The attribute was not found.";
+            }
+            else
+            {
+                return MyAttribute.Major + "." + MyAttribute.Minor;
+            }
+        }
+    }
+}
